2df36271690f70e2d19a99cc96f697755ea48d01
Yuri Salimovskiy <yuri@packettide.com>
2022-05-28 15:08:35 +0300

    update migration

--- database/migrations/2022_05_26_195300_donors.php
+++ database/migrations/2022_05_26_195300_donors.php
@@ -10,17 +10,20 @@ return new class () extends Migration {
         Schema::create('donors', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('telegraph_bot_id')->constrained('telegraph_bots')->cascadeOnDelete();
+            $table->foreignId('telegraph_bot_id')->constrained('telegraph_bots')->cascadeOnDelete(); //Chat ID in Telegram
 
-            $table->string('name')->nullable();
-            $table->string('phone')->nullable();
-            $table->enum('blood_type', ['', 'I (1)', 'II (2)', 'III (3)', 'IV (4)']);
-            $table->enum('blood_rh', ['', '+', '-']);
+            $table->unsignedTinyInteger('opt_in')->default('1'); //1 - active, 0 - temporary pause notifications
+
+            $table->string('name')->nullable(); // Name
+            $table->string('phone')->nullable(); // Phone number
+            $table->unsignedInteger('birth_year')->nullable(); // Year of birth to calculate age
+            $table->unsignedTinyInteger('weight_ok')->nullable(); // 1 - weight is over 55 kg
+            $table->unsignedTinyInteger('contras')->nullable(); // 1 - has contraindications, 0 - can be accepted as donor
+            $table->enum('blood_type', ['', 'I (1)', 'II (2)', 'III (3)', 'IV (4)']); // Blood type
+            $table->enum('blood_rh', ['', '+', '-']); // RH
 
             $table->date('last_request_date')->nullable();
             $table->date('last_donor_date')->nullable();
-
-            $table->int('sheet_row')->nullable();
 
             $table->timestamps();
 
