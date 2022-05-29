13bfc7694ed71d996ae653bd0d2c5dc37cea6f08
Yuri Salimovskiy <yuri@packettide.com>
2022-05-29 14:58:40 +0300

    no message

--- database/migrations/2022_05_28_172117_blood_requests.php
+++ database/migrations/2022_05_28_172117_blood_requests.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('blood_requests', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('owner_id')->constrained('users')->nullable()->nullOnDelete();
+            $table->foreignId('owner_id')->nullable()->constrained('users')->nullOnDelete();
 
             $table->unsignedTinyInteger('blood_type_id');
 
